c45b27fc73daa10de8e2f16340048fa6b3fce7e2
Manoj N <gokulakrishnan.k@tardiverse.com>
2023-05-22 18:37:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ require("dotenv").config();
 
 app.use(express.json());
 app.use("/user", router);
-app.use(cors());
+app.use(cors({
+    origin:"*"
+}));
 
 app.listen(process.env.SERVERPORT || 3000, () => {
   console.log("Server running in ", process.env.SERVERPORT);
