1385273f534fcb586be548e6fa2e300dbaf23d30
Manoj N <gokulakrishnan.k@tardiverse.com>
2023-05-22 19:58:18 +0530

    Update users.js

--- controller/users.js
+++ controller/users.js
@@ -3,19 +3,21 @@ const controller = {
   async createUser(req, res) {
     try {
       const { name, email, mobile } = req.body;
-      createPool.query(
+      const data= await createPool.query(
         `INSERT INTO users (name, email, mobile) VALUES ($1, $2, $3) RETURNING *`,
-        [name, email, mobile],
-        (err, result) => {
-          if (!err) {
-            console.log(`Data stored ${JSON.stringify(result.rows[0])}`);
-            res.status(200).json({ message: result.rows[0], status: true });
-          } else {
-            console.log(err);
-            res.status(403).json({ message: err, status: false });
-          }
-        }
+        [name, email, mobile]
+        // (err, result) => {
+        //   if (!err) {
+        //     console.log(`Data stored ${JSON.stringify(result.rows[0])}`);
+        //     res.status(200).json({ message: result.rows[0], status: true });
+        //   } else {
+        //     console.log(err);
+        //     res.status(403).json({ message: err, status: false });
+        //   }
+        // }
       );
+      console.log(data.rows)
+      res.status(200).json({ message: data.rows, status: true });
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: err, status: false });
