b24c6a83e9e04d93b6158a4a2c7498ffeaaf8e59
Manoj N <gokulakrishnan.k@tardiverse.com>
2023-05-22 20:27:28 +0530

    changed the server port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const router = require("./router/users");
 const cors = require("cors");
-// require("dotenv").config();
+require("dotenv").config();
 
 app.use(express.json());
 app.use("/user", router);
