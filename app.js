11a3598b81b6ae2229d224b271fef71ca76a5f48
Manoj N <gokulakrishnan.k@tardiverse.com>
2023-05-22 18:26:21 +0530

    cors added

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-const router = require("./router/users")
+const router = require("./router/users");
+const cors = require("cors");
 require("dotenv").config();
 
 app.use(express.json());
 app.use("/user", router);
-
+app.use(cors());
 
 app.listen(process.env.SERVERPORT || 3000, () => {
-  console.log("Server running in ",process.env.SERVERPORT);
+  console.log("Server running in ", process.env.SERVERPORT);
 });
