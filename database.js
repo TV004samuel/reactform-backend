b3ee6f86b7c5368fc5245f1f20894bf75cea650f
Manoj N <gokulakrishnan.k@tardiverse.com>
2023-05-23 19:01:27 +0530

    Update database.js

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const createPool = new Pool({
   user: "postgres",
-  host: "35.236.65.90",
+  host: "34.94.145.211",
   database: "postgres",
   password: "Tardiverse@123",
   port: 5432,
